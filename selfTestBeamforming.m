2235c97890e60cc0f804dc540d3bd136e31e6a2d
Ray <rc.w624@gmail.com>
2024-03-24 16:48:03 +0100

    add some notes to waste time

--- selfTestBeamforming.m
+++ selfTestBeamforming.m
@@ -1,27 +1,12 @@
 clear;
 load("polars.mat");
 
-f_sound = 1000;
+f_sound = 1000; % sound frequency for beamforming
+r = 0.1; % coordinate unit length
+c = 343.3; % speed of sound
 
-r = 0.1;
+% microphone system parameters definition
 m_pos = [r 0 0; r/2 -sqrt(3)/2*r 0; -r/2 -sqrt(3)/2*r 0; -r 0 0; -r/2 sqrt(3)/2*r 0; r/2 sqrt(3)/2*r 0]';
-%m_pos = [0 0 0; r 0 0; 0 -r 0; -r 0 0; 0 r 0; 2*r 0 0; 2*r/2 -sqrt(3)/2*2*r 0; -2*r/2 -sqrt(3)/2*2*r 0; -2*r 0 0; -2*r/2 sqrt(3)/2*2*r 0; 2*r/2 sqrt(3)/2*2*r 0; 3*r 0 0; 0 -3*r 0; -3*r 0 0; 0 3*r 0; 2*3*r 0 0; 2*3*r/2 -sqrt(3)/2*2*3*r 0; -2*3*r/2 -sqrt(3)/2*2*3*r 0; -2*3*r 0 0; -2*3*r/2 sqrt(3)/2*2*3*r 0; 2*3*r/2 sqrt(3)/2*2*3*r 0;]';
-%m_pos = [0 0 -3*r; 0 0 -r; 0 0 r; 0 0 3*r]';
-azimuth_deg = 0;
-elevation_deg = -90;
-azimuth = deg2rad(azimuth_deg);
-elevation = deg2rad(elevation_deg);
-s_pos = [cos(elevation)*cos(azimuth) cos(elevation)*sin(azimuth) sin(elevation)];
-
-c = 343.3;
-dasb_delay = s_pos*m_pos/c;
-
-dasb = exp(-1j*2*pi*f_sound*dasb_delay);
-
-sound_delay_angles = deg2rad(0:5:360);
-sound_delay_positions = [cos(sound_delay_angles')*cos(azimuth) cos(sound_delay_angles')*sin(azimuth) sin(sound_delay_angles')];
-sound_delays = sound_delay_positions*m_pos/c;
-
 temp = -1;
 for i=1:numel(polar_freq)
     if polar_freq(i) == f_sound
@@ -33,8 +18,24 @@ if temp == -1
 end
 sys = db2mag(repmat(polars(temp,:), numel(m_pos(1,:)), 1)');
 
+% sound source parameters definition
+azimuth_deg = 0;
+elevation_deg = -90;
+azimuth = deg2rad(azimuth_deg);
+elevation = deg2rad(elevation_deg);
+s_pos = [cos(elevation)*cos(azimuth) cos(elevation)*sin(azimuth) sin(elevation)];
+
+% delay and sum algorithm
+dasb_delay = s_pos*m_pos/c;
+dasb = exp(-1j*2*pi*f_sound*dasb_delay);
+
+% simulation parameters
+sound_delay_angles = deg2rad(0:5:360);
+sound_delay_positions = [cos(sound_delay_angles')*cos(azimuth) cos(sound_delay_angles')*sin(azimuth) sin(sound_delay_angles')];
+sound_delays = sound_delay_positions*m_pos/c;
 simulations = dasb*(exp(1j*2*pi*f_sound*sound_delays).*sys)'/numel(m_pos(1,:));
 
+% simulation polar plot
 threshold = -30;
 for i = 1:numel(sound_delay_angles)
     if 20*log10(abs(simulations(i))) < threshold
