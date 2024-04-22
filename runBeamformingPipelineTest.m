c8db417203d2bd35320afbe627a21e1ab9c95b15
zhaoaocheng <aocheng.zhao@student.kuleuven.be>
2024-04-22 12:50:47 +0200

    stft apply w_mwf

--- runBeamformingPipelineTest.m
+++ runBeamformingPipelineTest.m
@@ -19,25 +19,26 @@ for i = 1:iterations
     subplot (iterations, 1, i);
     plotSpec(input_stft(:,:,i),  'mag', xTickProp, yTickProp, cRange, 0); ylabel('f/kHz');
 end
-
+output_stft = zeros(size(input_stft));
 % % input frequency domain
-% input_f = fft(input_t);
-% input_len = numel(input_f(:,1));
-% f_start = 0;
-% f_end = 0;
-% output_f = zeros(size(input_f));
-% for i=1:numel(f_sound_group)
-%     f_start = f_end;
-%     if(i ~= numel(f_sound_group))
-%         f_end = (f_sound_group(1,i)+f_sound_group(1,i+1))/2;
-%     else
-%         f_end = fs_input/2;
-%     end
-%     i_start = round(f_start*input_len/fs_input) +1;
-%     i_end = round(f_end*input_len/fs_input);
-%     w_mwf = W_MWF{i};
-%     output_f(i_start:i_end,:) = input_f(i_start:i_end,:)*w_mwf.';
-% end
+f_start = 0;
+f_end = 0;
+frame_len = numel(input_stft(:,1,1));
+for i=1:numel(f_sound_group)
+    f_start = f_end;
+    if(i ~= numel(f_sound_group))
+        f_end = (f_sound_group(1,i)+f_sound_group(1,i+1))/2;
+    else
+        f_end = fs_input/2;
+    end
+    i_start = floor(f_start*frame_len/(fs_input/2)) +1;
+    i_end = floor(f_end*frame_len/fs_input);
+    w_mwf = W_MWF{i};
+    iterations = numel(input_stft(1,:,1));
+    for j = 1:iterations
+        output_stft(i_start:i_end,j,:) = squeeze(input_stft(i_start:i_end,j,:)) * w_mwf.';
+    end
+end
 % 
 % output_f = output_f(1:input_len/2,:);
 % 
@@ -50,7 +51,7 @@ end
 % % plot input
 % output_stft = calc_STFT(output_t, fs_input, win, N_STFT, R_STFT, 'onesided');
 
-output_stft = zeros(size(input_stft));
+% output_stft = zeros(size(input_stft));
 iterations = numel(input_stft(1,:,1));
 for i = 1:iterations
     output_stft(:,i,:) = squeeze(input_stft(:,i,:)) * W_MWF{6}.';
