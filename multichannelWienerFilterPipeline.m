ee68390afae1761394d13b04c4a6def99ec321f3
Ray <rc.w624@gmail.com>
2024-05-11 15:17:36 +0200

    pipeline pilelining

--- multichannelWienerFilterPipeline.m
+++ multichannelWienerFilterPipeline.m
@@ -16,7 +16,7 @@ for fileNr = 1:totalFileNr
     % load data
     % mwf coefficients calculation data time domain
     [N, fs_noiseOnly] = audioread(sprintf("Temporary/toBeTested/set%d/Noise (%d).flac", setNr, fileNr));
-    [Y, fs_targetPlusNoise]= audioread(sprintf("Temporary/toBeTested/set%d/Recording (%d).flac", setNr, fileNr));
+    [Y, fs_targetPlusNoise]= audioread(sprintf("Temporary/toBeTested/set%d/Y (%d).flac", setNr, fileNr));
     % mwf coefficients calculation data frequency domain
     N_stft = calc_STFT(N, fs_noiseOnly, win, N_STFT, R_STFT, 'onesided');
     Y_stft = calc_STFT(Y, fs_targetPlusNoise, win, N_STFT, R_STFT, 'onesided');
