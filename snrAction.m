fac557635e69f95c9570bb315cce268a5beba421
Ray <rc.w624@gmail.com>
2024-05-13 21:05:44 +0200

    snr clickGenerate

---
+++ snrAction.m
@@ -0,0 +1,34 @@
+beamImprovements = zeros(4, 1);
+for setNr = 1:2
+    if setNr == 1
+        totalFileNr = 18;
+    else
+        totalFileNr = 8;
+    end
+    for fileNr = 1:totalFileNr
+        input_t = audioread(sprintf("Temporary/toBeTested/set%d_Recording (%d).flac", setNr, fileNr));
+        N_in = audioread(sprintf("Temporary/toBeTested/set%d_N (%d).flac", setNr, fileNr));
+        
+        beamformerTypes = ["DS" "DSC" "MVDR" "MWF"];
+        for typeNr = 1:length(beamformerTypes)
+            output_t = audioread(sprintf("Temporary/toBeTested/out_%s/set%d_Recording (%d).flac", beamformerTypes(typeNr), setNr, fileNr));
+            N_out = audioread(sprintf("Temporary/toBeTested/out_%s/set%d_N (%d).flac", beamformerTypes(typeNr), setNr, fileNr));
+            noise_in_psd = pwelch(N_in);
+            noise_out_psd = pwelch(N_out);
+            y_psd = pwelch(input_t);
+            x_hat_psd = pwelch(output_t);
+            
+            signal_in_psd = mean(abs(y_psd)) - mean(abs(noise_in_psd));
+            input_SNR = 10*log10(sum(signal_in_psd)/sum(mean(abs(noise_in_psd))));
+            
+            signal_psd_out = mean(abs(x_hat_psd)) - mean(abs(noise_out_psd));
+            output_SNR = 10*log10(sum(signal_psd_out)/sum(mean(abs(noise_out_psd))));
+
+            snr_improvement = output_SNR - input_SNR;
+            beamImprovements(typeNr) = snr_improvement;
+        end
+        text = sprintf("& Rec.%d    & %.2f & %.2f & %.2f & %.2f & %.2f \\", fileNr, input_SNR, beamImprovements(1), beamImprovements(2), beamImprovements(3), beamImprovements(4));
+        disp(text);
+    end
+    
+end
